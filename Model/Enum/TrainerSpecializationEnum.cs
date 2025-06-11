8b61a12d3da8e9414c0abc41fa72072d9b91546a
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-06-11 12:37:23 +0300

    Added file. Closed #1

---
+++ Model/Enum/TrainerSpecializationEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace gym_project_business_logic.Model.Enum
+{
+    public enum TrainerSpecializationEnum
+    {
+        PersonalTraining = 0,
+        Dancing = 1,
+        Yoga = 2
+    }
+}
