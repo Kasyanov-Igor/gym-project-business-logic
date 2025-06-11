ea5b4773723914d2d8c656771e4f15530f9fe055
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-06-11 12:38:47 +0300

    Added file. Closed #1

---
+++ Services/Interface/IAdminService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using gym_project_business_logic.Model;
+using gym_project_business_logic.Model.Domains;
+
+namespace gym_project_business_logic.Services.Interface
+{
+    public interface IAdminService
+    {
+        public Task<bool> UpdateAdminAsync(int id, AdminDto adminDto);
+
+        public Task<Admin?> GetAdmin(string login, string password);
+
+        public Task<bool> GetEmail(string emailAddress);
+    }
+}
